12a672fdd7d0eb147cf39b620215dcf1d08979cf
Renatopais <57532862+Renatopais@users.noreply.github.com>
2020-01-14 18:31:46 +0000

    Auto Ren 14-01-2020

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/**
+ *	www.templatemo.com
+ */
+
+/* HTML document is loaded. DOM is ready.
+-----------------------------------------*/
+$(document).ready(function(){
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function(){
+		$('.templatemo-nav').slideToggle();
+	});
+
+	$( window ).resize(function() {
+		if($( window ).width() > 767) {
+			$('.templatemo-nav').show();
+		} else {
+			$('.templatemo-nav').hide();
+		}
+	});
+
+  // http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
+  $('body').bind('touchstart', function() {});
+
+});
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
